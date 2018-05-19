cd9c10a7691160f9a2237bc8c6a47c2fbf4946a4
Sheva Xu <shevaxu@outlook.com>
2018-05-19 23:28:56 +0800

    Add PID and mount namespaces, and root fs

--- main.go
+++ main.go
@@ -20,17 +20,42 @@ func help() {
 }
 
 func run() {
-	fmt.Printf("Running %v\n\n", os.Args[2:])
+	fmt.Printf("Running %v as parent PID %d\n\n", os.Args[2:], os.Getpid())
 
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
 	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		// UTS namespace gives its processes their own view of the system’s hostname and domain name
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS |
+			// MNT mount namespace gives the process’s contained within it their own mount table.
+			syscall.CLONE_NEWNS |
+			// PID namespace gives a process and its children their own view of a subset of the processes in the system.
+			// Think of it as a mapping table.
+			syscall.CLONE_NEWPID,
 	}
 	must(syscall.Sethostname([]byte("docker-0")))
 
 	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("Running %v as child PID %d\n\n", os.Args[2:], os.Getpid())
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+
+	// change the root filesystem
+	// (NOTE that /home/rootfs must be a valid dir, e.g., downloaded from
+	// https://github.com/wsilva/container-from-scratch-demo/blob/master/ubuntu-rootfs.tar.gz
+	must(syscall.Chroot("/home/rootfs"))
+	must(os.Chdir("/"))
+	// mount /proc so that `ps` can work
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+
+	must(cmd.Run())
+
+	must(syscall.Unmount("proc", 0))
 }
 
 func main() {
@@ -40,6 +65,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		fmt.Println(`Try "main run cmd [args]"`)
 	}
